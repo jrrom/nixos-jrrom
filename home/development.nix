d34a44915d1d7f8fb22051d14ec40697047f18ea
= <77691121+jrrom@users.noreply.github.com>
2025-01-25 23:31:48 +0530

    Final commit of the night.

--- home/development.nix
+++ home/development.nix
@@ -2,17 +2,29 @@
 
 {
   home.packages = with pkgs; [
-    # Basic
+    # Programming Languages
     clang
+    libgcc
+    go
+    luajit
 
+    # Development Kits
+    clang-tools
+    
     # Tools
     gdb
+    gnumake
+    shellcheck
     
     # Developer Environments
     devenv    
 
     # LSPS
+    vscode-langservers-extracted
+    gopls
+    lua-language-server
     basedpyright
+    bash-language-server
     nixd
   ];
 }
