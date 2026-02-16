737f19b91580ff4c3e72fa71dc3ebafec1d565d5
jrrom <77691121+jrrom@users.noreply.github.com>
2026-02-16 16:49:20 +0530

    feat: purescript mode and more

--- hosts/laptop/init.el
+++ hosts/laptop/init.el
@@ -413,6 +413,14 @@
           (css-mode        . css-ts-mode)
           (typescript-mode . tsx-ts-mode))))
 
+(use-package purescript-mode
+  :defer t
+  :config
+  (defun myhook-purescript-mode ()
+    (turn-on-purescript-indentation)
+    (add-hook 'before-save-hook #'purescript-sort-imports nil t))
+  (add-hook 'purescript-mode-hook #'myhook-purescript-mode))
+
 (use-package eglot
   :ensure nil
   :custom
@@ -522,18 +530,3 @@
   (interactive)
   (select-frame-set-input-focus (make-frame))
   (vterm))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("5a0ddbd75929d24f5ef34944d78789c6c3421aa943c15218bac791c199fc897d"
-     "8363207a952efb78e917230f5a4d3326b2916c63237c1f61d7e5fe07def8d378"
-     default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
